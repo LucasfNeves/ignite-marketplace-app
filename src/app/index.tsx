95c944e8dfc0de501fd0206503dc8a3d829d1705
LucasfNeves <lucasfarias.ln@gmail.com>
2026-02-09 11:50:51 -0300

    feat: adicionar tela de login e botão de navegação no App

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,16 @@
+import { router } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <TouchableOpacity onPress={() => router.push('/login')}>
+        <Text>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
