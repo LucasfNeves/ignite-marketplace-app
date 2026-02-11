612d458663ad3bfb63bad3cb0a251a8246e3ba00
Lucas Farias <lucasfarias.ln@gmail.com>
2026-02-11 14:30:19 -0300

    feat: adicionar suporte ao React Query com configuração do QueryClient

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,12 @@
 import { Stack } from 'expo-router';
 import '@/styles/global.css';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@/infrastructure/query/queryClient';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack />
+    </QueryClientProvider>
+  );
 }
